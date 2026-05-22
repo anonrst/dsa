70ddf7b868aa28a67f1b7e252b50020f7d601e5d
anon <mdayan1qaz@gmail.com>
2026-05-22 16:24:38 +0530

    A2Z sheet medium level took 5 min

---
+++ find_nth_root_of_a_number.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int NthRoot(int N, int M) {
+        int left = 0;
+        int right = M;
+        int ans = 1;
+        while (left <= right) {
+            int mid = left + (right  - left ) /2;
+            int val = 1;
+            for(int i = 0; i < N; i++){
+                val *= mid;
+                if(val > M) break;
+            }
+
+            if(val == M) return mid;
+            if(val < M){
+                left = mid + 1;
+            }else{
+                right = mid -1;
+            }
+        }
+        return -1;
+    }
+}
+
+
+public class find_nth_root_of_a_number {
+    
+    public static void main(String[] args) {
+        int result = new Solution().NthRoot(4, 16);
+        System.out.println(result == 2);
+    }
+}
