dd6e99324b90a16a3916fdf505c031e4ea85e3f9
anon <mdayan1qaz@gmail.com>
2026-05-22 15:47:11 +0530

    LC medium level question 69 with AR 41% took 8 min

---
+++ find_sqrt_of_a_number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public double floorSqrt(int n) {
+        int left = 1;
+        int right =n;
+        int ans = 0;
+        while (left <= right) {
+            int mid = left + (right - left) /2;
+            if((long)mid*mid <= n){
+                ans = mid;
+                left += 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
+
+public class find_sqrt_of_a_number {
+
+public static void main(String[] args) {
+    double n = new Solution().floorSqrt(15);
+    System.out.println(n);
+}
+}
