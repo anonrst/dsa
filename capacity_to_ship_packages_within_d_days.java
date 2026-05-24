6c88da4cbcdcdd2fe25fd392f212f8b734f5b698
anon <mdayan1qaz@gmail.com>
2026-05-24 10:29:05 +0530

    leetcode medium question with 73% rate took 10 min

---
+++ capacity_to_ship_packages_within_d_days.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int leastWeight = 0;
+        int maxWeight = 0;
+        for(int x: weights){
+            leastWeight = Math.max(leastWeight, x);
+            maxWeight += x;
+        }
+        int ans = 0;
+        while(leastWeight <= maxWeight){
+            int weightCapac = leastWeight + (maxWeight - leastWeight) /2;
+            if(canCompleteThisCapacity(weights,days, weightCapac)){
+                maxWeight = weightCapac - 1;
+                ans = weightCapac;
+            }else{
+                leastWeight = weightCapac + 1;
+            }
+        }
+        return ans;
+    }
+
+    boolean canCompleteThisCapacity(int[] packages, int days,int capacity){
+        int daysDone = 1;
+        int OneDayCapcacity = 0;
+
+        for(int x: packages){
+            if((OneDayCapcacity + x) > capacity){
+                daysDone++;
+                OneDayCapcacity = x;
+            }else{
+                OneDayCapcacity += x;
+            }
+            if(daysDone > days) return false;
+        }
+        return true;
+    }
+
+}
+public class capacity_to_ship_packages_within_d_days {
+public static void main(String[] args) {
+ int[] testcase = {3,2,2,4,1,4};
+ int result = new Solution().shipWithinDays(testcase, 3);
+ System.out.println(result == 6);   
+}
+    
+}
