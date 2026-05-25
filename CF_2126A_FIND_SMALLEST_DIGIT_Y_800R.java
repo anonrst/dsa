6c6715aeee99a087ba4733e04aaa0b88613f9f53
anonrst <mdayan1qaz@gmail.com>
2026-05-26 02:15:18 +0530

    codeforces 800 rating took 5 min

---
+++ CF_2126A_FIND_SMALLEST_DIGIT_Y_800R.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class CF_2126A_FIND_SMALLEST_DIGIT_Y_800R{
+public static void main(String[] args){
+Scanner s = new Scanner(System.in);
+int tests = s.nextInt();
+while(tests-- > 0){
+int number = s.nextInt();
+int smallestDigit = Integer.MAX_VALUE;
+
+while(number > 0){  
+  int digit = number % 10;  
+  smallestDigit = Math.min(smallestDigit, digit);  
+  number/= 10;  
+}  
+System.out.println(smallestDigit);
+
+}
+}
+}
