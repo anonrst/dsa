efe6ae8dd72e40de7cdac136b4ed7037a2e0fe17
anonrst <mdayan1qaz@gmail.com>
2026-05-26 06:58:37 +0530

    codeforces with rating 1000 took 30 min

--- CF_2126A_FIND_SMALLEST_DIGIT_Y_800R.java
+++ CF_2126A_FIND_SMALLEST_DIGIT_Y_800R.java
@@ -3,17 +3,16 @@ public class CF_2126A_FIND_SMALLEST_DIGIT_Y_800R{
 public static void main(String[] args){
 Scanner s = new Scanner(System.in);
 int tests = s.nextInt();
-while(tests-- > 0){
+while (tests-- > 0){
 int number = s.nextInt();
 int smallestDigit = Integer.MAX_VALUE;
 
-while(number > 0){  
-  int digit = number % 10;  
-  smallestDigit = Math.min(smallestDigit, digit);  
-  number/= 10;  
-}  
+while (number > 0){
+int digit = number % 10;
+smallestDigit = Math.min(smallestDigit, digit);
+number /= 10;
+}
 System.out.println(smallestDigit);
-
 }
 }
 }
---
+++ CF_2217A_THE_EQUALIZER_100R.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class CF_2217A_THE_EQUALIZER_100R {
+  public static void main(String[] atgs) {
+    Scanner s = new Scanner(System.in);
+    int tests = s.nextInt();
+    while (tests-- > 0) {
+      int[] LenAndK = new int[2];
+      LenAndK[0] = s.nextInt();
+      LenAndK[1] = s.nextInt();
+      int k = LenAndK[1];
+      int[] nums = new int[LenAndK[0]];
+      int sum = 0;
+      for (int i = 0; i < LenAndK[0]; i++) {
+        nums[i] = s.nextInt();
+        sum += nums[i];
+      }
+      if (sum % 2 == 0) {
+        int newSum = (LenAndK[0])*k; //now all the element xecelt at index zero can be rrplaced by k, becausr of consttaint     ai(1≤i≤n)
+        boolean isEvenK = newSum % 2 == 0;
+        if (isEvenK) {
+          System.out.println("YES");
+        } else {
+          System.out.println("NO");
+        }
+      } else {
+        System.out.println("YES");
+      }
+    }
+  }
+}
