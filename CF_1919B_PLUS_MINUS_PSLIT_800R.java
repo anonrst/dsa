b6f1b15c3207e56efba76ecac91cbf1a322b5213
anonrst <mdayan1qaz@gmail.com>
2026-05-27 18:58:03 +0530

    codeforces again  with 800R took 10 min

---
+++ CF_1919B_PLUS_MINUS_PSLIT_800R.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class CF_1919B_PLUS_MINUS_PSLIT_800R {
+  public static void main(String[] args){
+    Scanner s = new java.util.Scanner(System.in);
+    int tests = s.nextInt();
+    while(tests-- > 0){
+      int A = s.nextInt();
+      int resilt = 0;
+      String ss = s.next();
+      char[] si = ss.toCharArray();
+      for(int i = 0; i < A; i++){
+        char c = si[i];
+        if(c == '+'){
+          resilt++;
+        }else{
+          resilt--;
+        }
+      }
+      if(resilt < 0){
+        System.out.println(-resilt);
+      }else{
+        System.out.println(resilt);
+      }
+    }
+  }
+}
